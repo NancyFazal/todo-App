18e0b2ca0740303eb24620b97911fbb5583d7f5f
FazalNancy <nancy.fazal@arbonaut.com>
2025-01-03 15:15:50 +0200

    Components restructuring and use of Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import AddTodo from "./components/AddTodo";
 import AppName from "./components/AppName";
-import TodoItem1 from "./components/TodoItem1";
-import TodoItem2 from "./components/TodoItem2";
+import TodoItems from "./components/TodoItems";
+
 import "./App.css";
 
 function App() {
+  const todoItems = [
+    {
+      name: "React Context API",
+      dueDate: "03/01/2025",
+    },
+    {
+      name: "React Hooks",
+      dueDate: "03/01/2025",
+    },
+    {
+      name: "Redux",
+      dueDate: "03/01/2025",
+    },
+  ];
   return (
     <center className="todo-container">
       <AppName />
       <AddTodo />
-      <div className="items-container">
-        <TodoItem1 />
-        <TodoItem2 />
-      </div>
+      <TodoItems todoItems={todoItems}></TodoItems>
     </center>
   );
 }
