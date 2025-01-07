a5f571453ab5ab5b2659e64e9ca6481d01330a1a
FazalNancy <nancy.fazal@arbonaut.com>
2025-01-07 20:33:46 +0200

    CSS modules added

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,7 +1,8 @@
 import TodoItem from "./TodoItem";
+import styles from "./TodoItems.module.css";
 const TodoItems = ({ todoItems }) => {
   return (
-    <div className="items-container">
+    <div className={styles.itemsContainer}>
       {todoItems.map((item) => (
         <TodoItem todoDate={item.dueDate} todoName={item.name} />
       ))}
